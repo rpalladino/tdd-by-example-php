d5e2fa688eaa47c8edc021a9b3dccbc45cd6fbe5
Rocco Palladino <rpalladino@gmail.com>
2016-08-17 23:52:45 -0500

    Test dollar side effects

--- tests/DollarTest.php
+++ tests/DollarTest.php
@@ -7,5 +7,7 @@ class DollarTest extends PHPUnit_Framework_TestCase
 		$five = new Dollar(5);
 		$five->times(2);
 		$this->assertEquals(10, $five->amount);
+		$five->times(3);
+		$this->assertEquals(15, $five->amount);
 	}
 }
