938bdbfa0f107abf5dc4b857f7b04cffd6496317
Rocco Palladino <rpalladino@gmail.com>
2016-08-18 00:22:22 -0500

    Rewrite the first assertion to compare Dollars to Dollars

--- tests/DollarTest.php
+++ tests/DollarTest.php
@@ -6,7 +6,7 @@ class DollarTest extends PHPUnit_Framework_TestCase
 	{
 		$five = new Dollar(5);
 		$product = $five->times(2);
-		$this->assertEquals(10, $product->amount);
+		$this->assertEquals(new Dollar(10), $product);
 		$product = $five->times(3);
 		$this->assertEquals(15, $product->amount);
 	}
