f33b91e8e86c0a92fd9ae591814b7f03843ace9b
Rocco Palladino <rpalladino@gmail.com>
2016-08-18 19:49:23 -0500

    Copy and edit Dollar class for Franc

---
+++ src/Franc.php
@@ -0,0 +1,21 @@
+<?php
+
+class Franc
+{
+    private $amount;
+
+	public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+	}
+
+	public function times(int $multiplier): Franc
+    {
+        return new Franc($this->amount * $multiplier);
+    }
+
+    public function equals(Franc $franc)
+    {
+        return $this->amount === $franc->amount;
+    }
+}
