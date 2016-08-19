8efb4b4b5ecfe2b372ce6021e8c2a2559629aa2e
Rocco Palladino <rpalladino@gmail.com>
2016-08-19 00:32:39 -0500

    There are no references to Dollar, so we can remove it

--- src/Dollar.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Dollar extends Money
-{
-}
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -2,8 +2,17 @@
 
 class MoneyTest extends PHPUnit_Framework_TestCase
 {
+	public function testMultiplication()
+	{
+		$five = Money::dollar(5);
+		$this->assertEquals(Money::dollar(10), $five->times(2));
+		$this->assertEquals(Money::dollar(15), $five->times(3));
+	}
+
 	public function testEquality()
 	{
+		$this->assertTrue((Money::dollar(5))->equals(Money::dollar(5)));
+		$this->assertFalse((Money::dollar(5))->equals(Money::dollar(6)));
 		$this->assertNotEquals(Money::franc(5), Money::dollar(5));
 		$this->assertFalse((Money::franc(5))->equals(Money::dollar(5)));
 	}
