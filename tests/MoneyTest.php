ed5aebfdcd31b991d08d93321e862f89ec397a88
Rocco Palladino <rpalladino@gmail.com>
2016-08-19 00:36:37 -0500

    Remove redundant tests

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -2,24 +2,25 @@
 
 class MoneyTest extends PHPUnit_Framework_TestCase
 {
-	public function testMultiplication()
+	public function testDollarMultiplication()
 	{
 		$five = Money::dollar(5);
 		$this->assertEquals(Money::dollar(10), $five->times(2));
 		$this->assertEquals(Money::dollar(15), $five->times(3));
 	}
 
+	public function testFrancMultiplication()
+	{
+		$five = Money::franc(5);
+		$this->assertEquals(Money::franc(10), $five->times(2));
+		$this->assertEquals(Money::franc(15), $five->times(3));
+	}
+
 	public function testEquality()
 	{
 		$this->assertTrue((Money::dollar(5))->equals(Money::dollar(5)));
 		$this->assertFalse((Money::dollar(5))->equals(Money::dollar(6)));
-		$this->assertNotEquals(Money::franc(5), Money::dollar(5));
 		$this->assertFalse((Money::franc(5))->equals(Money::dollar(5)));
-	}
-
-	public function testDifferentClassEquality()
-	{
-		$this->assertTrue((new Money(10, "CHF"))->equals(new Franc(10, "CHF")));
 	}
 
 	public function testCurrency()
