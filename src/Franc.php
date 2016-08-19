574a390441648eccf6574d2d523441882d72b690
Rocco Palladino <rpalladino@gmail.com>
2016-08-18 23:32:38 -0500

    Reconcile two implementations of times() by making them both return Money

--- src/Franc.php
+++ src/Franc.php
@@ -7,7 +7,7 @@ class Franc extends Money
         $this->amount = $amount;
 	}
 
-	public function times(int $multiplier): Franc
+	public function times(int $multiplier): Money
     {
         return new Franc($this->amount * $multiplier);
     }
