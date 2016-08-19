21fb8527a36ea41b965101eba0741fb5a649355d
Rocco Palladino <rpalladino@gmail.com>
2016-08-18 23:43:07 -0500

    Use factory method everywhere in the tests

--- tests/DollarTest.php
+++ tests/DollarTest.php
@@ -5,13 +5,13 @@ class DollarTest extends PHPUnit_Framework_TestCase
 	public function testMultiplication()
 	{
 		$five = Money::dollar(5);
-		$this->assertEquals(new Dollar(10), $five->times(2));
-		$this->assertEquals(new Dollar(15), $five->times(3));
+		$this->assertEquals(Money::dollar(10), $five->times(2));
+		$this->assertEquals(Money::dollar(15), $five->times(3));
 	}
 
 	public function testEquality()
 	{
-		$this->assertTrue((new Dollar(5))->equals(new Dollar(5)));
-		$this->assertFalse((new Dollar(5))->equals(new Dollar(6)));
+		$this->assertTrue((Money::dollar(5))->equals(Money::dollar(5)));
+		$this->assertFalse((Money::dollar(5))->equals(Money::dollar(6)));
 	}
 }
