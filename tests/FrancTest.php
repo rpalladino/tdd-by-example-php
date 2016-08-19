89ef82fabcc05f8550ebf9b71af2fbd704a6db28
Rocco Palladino <rpalladino@gmail.com>
2016-08-18 22:31:05 -0500

    Test for Franc equality

--- tests/FrancTest.php
+++ tests/FrancTest.php
@@ -8,4 +8,10 @@ class FrancTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(new Franc(10), $five->times(2));
 		$this->assertEquals(new Franc(15), $five->times(3));
 	}
+
+	public function testEquality()
+	{
+		$this->assertTrue((new Franc(5))->equals(new Franc(5)));
+		$this->assertFalse((new Franc(5))->equals(new Franc(6)));
+	}
 }
