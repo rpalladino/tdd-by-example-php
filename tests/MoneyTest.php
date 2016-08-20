40b4ab3d797a16dfcb8872058bd3ffbd74950597
Rocco Palladino <rpalladino@gmail.com>
2016-08-20 06:54:40 -0500

    Combine tests

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -38,4 +38,32 @@ class MoneyTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals($five, $sum->augend);
 		$this->assertEquals($five, $sum->addend);
 	}
+
+	public function testReduceSum()
+	{
+		$sum = new Sum(Money::dollar(3), Money::dollar(4));
+		$bank = new Bank();
+		$result = $bank->reduce($sum, "USD");
+		$this->assertEquals(Money::dollar(7), $result);
+	}
+
+	public function testReduceMoney()
+	{
+		$bank = new Bank();
+		$result = $bank->reduce(Money::dollar(1), "USD");
+		$this->assertEquals(Money::dollar(1), $result);
+	}
+
+	public function testReduceMoneyDifferentCurrencies()
+	{
+		$bank = new Bank();
+		$bank->addRate("CHF", "USD", 2);
+		$result = $bank->reduce(Money::franc(2), "USD");
+		$this->assertEquals(Money::dollar(1), $result);
+	}
+
+	public function testIdentityRate()
+	{
+		$this->assertEquals(1, (new Bank)->rate("USD", "USD"));
+	}
 }
