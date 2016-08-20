b4de56107d6e36a0050d964635f41926ade50c27
Rocco Palladino <rpalladino@gmail.com>
2016-08-20 07:57:19 -0500

    Test for Sum::times()

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -87,4 +87,15 @@ class MoneyTest extends PHPUnit_Framework_TestCase
 		$result = $bank->reduce($sum, "USD");
 		$this->assertEquals(Money::dollar(15), $result);
 	}
+
+	public function testSumTimes()
+	{
+		$fiveBucks = Money::dollar(5);
+		$tenFrancs = Money::franc(10);
+		$bank = new Bank();
+		$bank->addRate("CHF", "USD", 2);
+		$sum = (new Sum($fiveBucks, $tenFrancs))->times(2);
+		$result = $bank->reduce($sum, "USD");
+		$this->assertEquals(Money::dollar(20), $result);
+	}
 }
