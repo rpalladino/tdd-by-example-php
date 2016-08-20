f8e184aa03f3a9d436b7fe7d690b145a973f73b8
Rocco Palladino <rpalladino@gmail.com>
2016-08-20 06:06:39 -0500

    ...then we can eliminate those ugly class checks.

--- src/Bank.php
+++ src/Bank.php
@@ -4,11 +4,6 @@ class Bank
 {
 	public function reduce(Expression $source, string $to): Money
 	{
-		if ($source instanceof Money) {
-			return $source->reduce($to);
-		}
-
-		$sum = $source;
-		return $sum->reduce($to);
+		return $source->reduce($to);
 	}
 }
