aefb39aab770e02613b3aae5a8cb46a2b50c267e
Rocco Palladino <rpalladino@gmail.com>
2016-08-20 06:13:18 -0500

    We have two francs and we want a dollar. That sounds like a test case.

--- tests/BankTest.php
+++ tests/BankTest.php
@@ -16,4 +16,12 @@ class BankTest extends PHPUnit_Framework_TestCase
 		$result = $bank->reduce(Money::dollar(1), "USD");
 		$this->assertEquals(Money::dollar(1), $result);
 	}
+
+	public function testReduceMoneyDifferentCurrencies()
+	{
+		$bank = new Bank();
+		$bank->addRate("CHF", "USD", 2);
+		$result = $bank->reduce(Money::franc(2), "USD");
+		$this->assertEquals(Money::dollar(1), $result);
+	}
 }
