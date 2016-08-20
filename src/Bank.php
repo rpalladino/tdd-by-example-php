77b9bfe5a7ce982c137d4506d25710db84659438
Rocco Palladino <rpalladino@gmail.com>
2016-08-20 06:01:07 -0500

    How to test and implement Bank::reduce() when the argument is a Money.

--- src/Bank.php
+++ src/Bank.php
@@ -4,6 +4,10 @@ class Bank
 {
 	public function reduce(Expression $source, string $to): Money
 	{
+		if ($source instanceof Money) {
+			return $source;
+		}
+
 		$sum = $source;
 		return $sum->reduce($to);
 	}
--- tests/BankTest.php
+++ tests/BankTest.php
@@ -9,4 +9,11 @@ class BankTest extends PHPUnit_Framework_TestCase
 		$result = $bank->reduce($sum, "USD");
 		$this->assertEquals(Money::dollar(7), $result);
 	}
+
+	public function testReduceMoney()
+	{
+		$bank = new Bank();
+		$result = $bank->reduce(Money::dollar(1), "USD");
+		$this->assertEquals(Money::dollar(1), $result);
+	}
 }
