4b2a83948c7093ba189a11b3b4b85d0629010e85
Rocco Palladino <rpalladino@gmail.com>
2016-12-24 12:47:43 -0600

    Replace wasRun flag with logging

--- xunit/src/WasRun.php
+++ xunit/src/WasRun.php
@@ -4,18 +4,12 @@ namespace xUnit;
 
 class WasRun extends TestCase {
     public $log;
-    public $wasRun;
-
-    public function __construct($name) {
-        $this->wasRun = false;
-        parent::__construct($name);
-    }
 
     public function setUp() {
         $this->log = "setUp ";
     }
 
     public function testMethod() {
-        $this->wasRun = true;
+        $this->log += "testMethod ";
     }
 }
